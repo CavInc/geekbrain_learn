e39eebc1ff3d968979716fbf9685c47b57beb997
cav <cavinc20@gmail.com>
2016-01-22 00:33:49 +0700

    Добавлены файлы проекта

---
+++ learn1.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+function createTable(container_id,row,col){
+	
+}
+/**
+ */
+function createCheckTable(container_id){
+}
+
+window.onload=function(){
+	console.log('TEST');
+	var l1=document.getElementById('paragraf1');
+}
