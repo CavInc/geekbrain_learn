b7509e10563fd6dcb2818d525023339deb0ffd2b
cav <cavinc20@gmail.com>
2016-01-31 22:06:43 +0700

    Работа над уроком 4

--- L4/learn4.js
+++ L4/learn4.js
@@ -1,17 +1,24 @@
 /**
  * 
  */
-function TabPane(container_id){
-  this.container_id=container_id;
+function TabPane(container_id,tabed_id){
+  this.container_id = container_id;
+  this.tabed_id = tabed_id;
   
-  this.addTab(title){
-    
+  this.addTab = function(title){
+    if ($(this.container_id).find("#"+this.tabed_id).length===0){
+      $(this.container_id).html("<div id='"+this.tabed_id+"'></div>");
+      $(this.container_id).find("#"+this.tabed_id).html('<div id="'+this.tabed_id+'_table" class="horizontal tabed_header"></div>');
+    }
+    $("#"+this.tabed_id+"_table").append($("<p>"+title+"</p>"));
   };
-  this.delTab(index){
+  this.delTab = function(index){
+  };
+  this.setContentTab = function(index,content) {
   };
   
   this.countTab=0;
-  
+  this.selectIndex=-1;
   
   
 }
@@ -19,8 +26,9 @@ function TabPane(container_id){
 (function($){
   $(function(){
      $("#container").after("<p> а ебать копать</p>");
-     var tabpane=new TabPane('#container');
-     tabpane("Первая закладка");
+     var tabpane=new TabPane('#container','tabpane1');
+     tabpane.addTab("Первая закладка");
+     tabpane.addTab("Вторая закладка");
      
   });
 })(jQuery);
