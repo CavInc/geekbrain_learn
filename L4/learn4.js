aa7399fc999eecd5823200ce0dd5c1b12df56364
cav <cavinc20@gmail.com>
2016-02-01 22:15:54 +0700

    Работа над ДЗ 4

--- L4/learn4.js
+++ L4/learn4.js
@@ -11,10 +11,26 @@ function TabPane(container_id,tabed_id){
       $(this.container_id).find("#"+this.tabed_id).html('<div id="'+this.tabed_id+'_table" class="horizontal tabed_header"></div>');
     }
     $("#"+this.tabed_id+"_table").append($("<p>"+title+"</p>"));
+    if (this.countTab==0){
+      $("#"+this.tabed_id+"_table > p ").addClass("active");
+    }
+    $("#"+this.tabed_id+"_table > p").click(function(event){
+      console.log(event);
+      $("#"+this.tabed_id+"_table > p .active").removeClass("active");
+      
+    });
+    $("#"+this.tabed_id).append($('<div id="table_context'+this.countTab+'" class="tabed_body"></div>'));
+    if (this.countTab!=0) {
+      $("#table_context"+this.countTab).hide();
+    }
+    this.countTab +=1;
+
   };
   this.delTab = function(index){
+    
   };
   this.setContentTab = function(index,content) {
+    $("#table_context"+index).html(content);
   };
   
   this.countTab=0;
@@ -28,7 +44,9 @@ function TabPane(container_id,tabed_id){
      $("#container").after("<p> а ебать копать</p>");
      var tabpane=new TabPane('#container','tabpane1');
      tabpane.addTab("Первая закладка");
+     tabpane.setContentTab(0,"<p> А это у нас первая вкладка ");
      tabpane.addTab("Вторая закладка");
+     tabpane.setContentTab(1,"<p> А это у нас вторая  вкладка ");
      
   });
 })(jQuery);
