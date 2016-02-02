2feb6e0db3421fba3c2f358ae0ea74f510c10465
cav <cavinc20@gmail.com>
2016-02-02 23:39:03 +0700

    Сделано переключение закладок включая контекст стили и прочие красявости не сделаны. Есть странный повторыный вызов процедуры обработки клика подумать почему так.

--- L4/learn4.js
+++ L4/learn4.js
@@ -10,18 +10,29 @@ function TabPane(container_id,tabed_id){
       $(this.container_id).html("<div id='"+this.tabed_id+"'></div>");
       $(this.container_id).find("#"+this.tabed_id).html('<div id="'+this.tabed_id+'_table" class="horizontal tabed_header"></div>');
     }
-    $("#"+this.tabed_id+"_table").append($("<p>"+title+"</p>"));
+    $("#"+this.tabed_id+"_table").append($("<p id="+this.countTab+">"+title+"</p>"));
     if (this.countTab==0){
       $("#"+this.tabed_id+"_table > p ").addClass("active");
-    }
+    };
+    
     $("#"+this.tabed_id+"_table > p").click(function(event){
-      $("#"+this.parentElement.getAttribute('id')+" > p.active").removeClass("active");
-      event.currentTarget.className='active';
+      $("#"+$(this).parent().attr('id')+" > p.active").removeClass("active");
+      $(this).addClass('active');
       
+      console.log($(this).attr('id'));
+      
+      $(".active_context").hide();
+      $(".active_context").removeClass("active_context");
+      var elm=$("#table_context"+$(this).attr('id'));
+      elm.addClass('active_context');
+      elm.show();
     });
+    
     $("#"+this.tabed_id).append($('<div id="table_context'+this.countTab+'" class="tabed_body"></div>'));
     if (this.countTab!=0) {
       $("#table_context"+this.countTab).hide();
+    } else {
+      $(this.container_id+" #table_context0").addClass('active_context')
     }
     this.countTab +=1;
 
@@ -41,16 +52,13 @@ function TabPane(container_id,tabed_id){
 
 (function($){
   $(function(){
-     $("#container").after("<p> а ебать копать</p>");
      var tabpane=new TabPane('#container','tabpane1');
      tabpane.addTab("Первая закладка");
-     tabpane.setContentTab(0,"<p> А это у нас первая вкладка ");
+     tabpane.setContentTab(0,"<p> А это у нас первая вкладка </p>");
      tabpane.addTab("Вторая закладка");
-     tabpane.setContentTab(1,"<p> А это у нас вторая  вкладка ");
-     
+     tabpane.setContentTab(1,"<p> А это у нас вторая  вкладка </p>");
+     tabpane.addTab("А еще одна закладка");
+     tabpane.setContentTab(2,"<p> Пусть будет это третьей закладкой </p>");     
   });
 })(jQuery);
 
-window.onload=function(){
-
-}
